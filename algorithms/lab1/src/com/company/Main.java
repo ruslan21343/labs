c9e3086a2c9740be69a4389179ae4d194c28d0d3
RHumeniuk <Ruslan.Humeniuk@onseo.biz>
2021-09-14 14:24:25 +0300

    Added lab1 Algorithms and data structures

--- algorithms/lab1/src/com/company/Main.java
+++ algorithms/lab1/src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int multiplication = a * b;
+        int number = Math.floorDiv(multiplication, c);
+
+        if (number > 100) {
+            System.out.println("Such a nice number");
+        } else {
+            System.out.println("Also nice number ;)");
+        }
+
     }
+
 }
